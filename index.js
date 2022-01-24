c943add9c7f47bbaa1051203d7ec47fd33a70c81
Kirill <freshjs.ftd.@gmail.com>
2022-01-24 20:03:35 +0200

    Add variables

---
+++ index.js
@@ -0,0 +1,28 @@
+// alert( 50 );
+// alert('test');
+
+/*
+ var 
+ let
+*/
+// 1. Создать переменную
+let test;  // обьявление переменной
+// 2. Записать в неё данные
+test = 'Hello World'; // запись данных в переменную
+// 3. вывести данные в консоль
+console.log(test);
+// 4. перезаписать данные
+test = 'text';
+// 5. вывести их в консоль еще раз
+console.log(test);
+let testVariableWithSomethingYes = true; // инициализация переменной
+
+let _; // lodash
+let $; // jQuery
+
+const birthday = 40;
+console.log(birthday);
+console.log(birthday);
+
+// console.log(null);
+// console.log('true story 1');
