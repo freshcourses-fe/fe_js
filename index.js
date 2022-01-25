670392d95b25883bd1aabb37cf450653289d3ce6
Kirill <freshjs.ftd.@gmail.com>
2022-01-25 20:48:27 +0200

    Add comma operator

--- index.js
+++ index.js
@@ -38,3 +38,13 @@ let num2 = '1';
 console.log(num1 !== num2);
 
 console.log(NaN === NaN);
+
+// let x1= 1, x2= 2, x3=3, x4='test', x5;
+
+console.log(5 , 7);
+
+let test = 5;
+
+test = (test++, false , 2+2 , 12);
+
+console.log(test);
