4fa590b030cb8973a61f4bd7f3e6ffdc002ad254
Kirill <freshjs.ftd.@gmail.com>
2022-01-25 19:14:20 +0200

    Add unary operators

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const result = (-4) ** 2;
 let num = 10;
 
 // num = num + 1;
-console.log(num);
+// console.log(num);
 
 
-console.log(++num);
+// console.log(++num);
 
-console.log(num++);
+// console.log(num++);
 
-console.log(--num);
+// console.log(--num);
 
 
+let number = '124';
+
+console.log(+number);
+console.log(-number);
 
