85dbdcf1571227dd8cf3b688d3fca5049c4bc9d5
Kirill <freshjs.ftd.@gmail.com>
2022-01-25 19:05:53 +0200

    Add increment/decrement

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const mod = -4 % 2;
 const result = (-4) ** 2;
-// alert();
 
-console.log(result);
+let num = 10;
+
+// num = num + 1;
+console.log(num);
+
+
+console.log(++num);
+
+console.log(num++);
+
+console.log(--num);
+
+
+
