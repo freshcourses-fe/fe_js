e54ad04d725d00d08ac05df607781f6d911042ab
Kirill <freshjs.ftd.@gmail.com>
2022-01-26 18:59:30 +0200

    Add very bad pseudo calculator

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const userName = prompt('Enter your name');
-const greeting = "Hello " + userName;
-alert(greeting);
+const input1 = +prompt('Enter num 1');
+const input2 = +prompt('Enter num 2');
+
+const result =  input1 + input2;
+
+const checkResult = isNaN(result);
+
+if(checkResult === true){
+  alert('ERROR');
+} else {
+  alert(result);
+}
