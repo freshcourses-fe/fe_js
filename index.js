1f9efa46df69eb5f656417ee13730cd22a88d077
Kirill <freshjs.ftd.@gmail.com>
2022-01-27 19:59:25 +0200

    Add bad var behaivior

--- index.js
+++ index.js
@@ -14,3 +14,66 @@ const findMinNumberFromTwo = function (num1, num2) {
   }
   return num1;
 };
+
+
+// if( ' ' ) {
+//   alert('два больше нуля')
+// }
+
+// const greetUser = function(userName) {
+//   if(userName) {
+//     alert('Hello ' + userName);
+//   } else {
+//     alert('Hello Anonym');
+//   }
+// }
+
+
+
+// const checkNumberAgainstOther = function (num1, num2) {
+//   if(typeof num1 === 'number' ) {
+//     if (num1 > num2) {
+//       console.log(num1 +' больше ' + num2);
+//     } else if (num1 < num2) {
+//       console.log(num1 +" меньше " + num2);
+//     } else if (num2 > num1) {
+//       console.log('test');
+//     } else {
+//       console.log(num1 +' равны ' + num2);
+//     }
+
+//   } else {
+//     console.log('Введите числа плз');
+//   }
+// };
+
+const num1 = 5;
+
+if(true) {
+  var test1 = true;
+  console.log(test1);
+  console.log(num1);
+
+  if(true) {
+    const test2 = 12;
+    console.log(test2);
+    console.log(test1);
+    console.log(num1);
+  }
+
+}
+
+const testFunc = function() {
+  var x = true;
+
+  if(true) {
+    var y = true;
+  }
+
+  console.log(y);
+}
+
+
+
+console.log(y);
+// console.log(num1);
