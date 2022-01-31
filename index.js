f9ddeacd7bd6d892e367751b3e2aeca5530b124d
Kirill <freshjs.ftd.@gmail.com>
2022-01-31 18:16:20 +0200

    Add ternary example

--- index.js
+++ index.js
@@ -10,22 +10,38 @@ function getMinNumber2(number1, number2) {
   return number1 < number2 ? number1 : number2;
 }
 
-const result1 = getMinNumber(1, 5);
-
-let counter = 1;
-
-while (true) {
-  console.log(counter++);
-
-  if (counter > 5) {
-    break;
-  }
+if(true) {
+  // 
+  'block 1'
+} else {
+  //
+  'block 2'
 }
+// true ? 'block 1' : 'block 2';
 
-do {
-  console.log(counter++);
+const userName = prompt('Enter your name');
+const isMale = confirm('Вы мужчина?');
 
-  if (counter > 100) {
-    break;
-  }
-} while (true);
+const prefix = isMale ? 'Mr. ' : 'Mrs. ';
+const message = 'Hello ' + prefix + userName;
+alert(message);
+
+// const result1 = getMinNumber(1, 5);
+
+// let counter = 1;
+
+// while (true) {
+//   console.log(counter++);
+
+//   if (counter > 5) {
+//     break;
+//   }
+// }
+
+// do {
+//   console.log(counter++);
+
+//   if (counter > 100) {
+//     break;
+//   }
+// } while (true);
