17d35c319fe47e243e516f89cc8b3ecf13ed626a
Kirill <freshjs.ftd.@gmail.com>
2022-02-02 20:55:37 +0200

    Add property accessors and template strings

--- index.js
+++ index.js
@@ -1,168 +1,69 @@
 'use strict';
 
-// let myFirstName = 'Kirill';
-// let myLastName = 'Secret';
+let number1 = 5;
+let number2 = 3;
+// let number1 = 5;
+// let number2 = 3;
+// const answer = prompt(number1 + ' + ' + number2 + ' ?');
 
-// let student1FirstName = 'Petya';
-// let student1LastName = 'Petrov';
+// alert(number1 + ' + '+ number2 +' = ' + (number1 + number2));
+// alert(`${number1} + ${number2} = ${number1 + number2}`);
+`sdsadadsadsadsdsad + ${5 + 2 > 7}`;
 
-// let student2FirstName = 'Natasha';
-// let student2LastName = 'Sidorova';
+// alert("Глава 1 \nЧто-то по'шло не так. Иван сказал: \"Все плохо\u00AE\"");
 
-// function greet(firstName, lastName) {
-//   return 'Hello '+ firstName + ' ' + lastName
-// }
+// alert(`Глава
+// Что-то по'шло не так.
+//             Иван сказал: "Все плохо\u00AE"`);
 
-const teacher = {
-  firstName: 'Kiril',
-  lastName: 'Secret',
-  weight: 900,
-  isMale: true,
-  nullProperty: null,
-  undefinedProperty: undefined,
-  address: {
-    country: 'UA',
-    city: 'ZP',
-    street: 'nope',
-  },
-  sayHello: function () {
-    alert('Привет');
-  },
+// const answer = prompt(number1 + ` + ` + number2 + ` ?`);
+
+const user = {
+  login: 'test',
+  password: '12345',
+  10: 'secret 1',
+  'secret string': true,
 };
 
-const student1 = {
-  firstName: 'Kirill',
-  lastName: 'Petrov',
-  weight: 85,
-};
-
-// ERROR!
-// teacher = {
-//   firstName: 'Kirill',
-//   lastName: 'Petrov',
-//   weight: 85
-// }
-
-// перезапись свойств обьекта
-// или создание новых свойств
-// уже существующему обьекту
-teacher.firstName = 'Vasya';
-
-// удаление свойства
-delete teacher.address;
-
-// не алертите обьекты
-// alert(student1);
-
-function greet(person) {
-  if (
-    typeof person.firstName === 'string' &&
-    person.firstName !== '' &&
-    typeof person.lastName === 'string' &&
-    person.lastName !== ''
-  ) {
-    return 'Hello ' + person.firstName + ' ' + person.lastName;
-  }
-  return 'Неверные данный, нужен пользователь';
-}
-
-const cat1 = {
-  name: 'Pushok',
-  breed: 'Maine Coon',
-  age: 3,
-  isMale: true,
-  meow: function () {
-    return 'Meow';
-  },
-};
-
-const cat2 = {
-  name: 'Vas`ka',
-  breed: 'Persian',
-  age: 2,
-  isMale: true,
-  meow: function () {
-    return 'Meow';
-  },
-};
-
-const cat3 = {
-  name: 'Pushok',
-  brd: 'Maine Coon',
-  age: 3,
-  isMale: true,
-  meow: function () {
-    return 'Meow';
-  },
-};
-
-function Cat(name, breed, age, isMale) {
-  // debugger;
-  this.meow = function () {
-    return 'Meow';
-  };
-
-  this.name = name;
-  this.breed = breed;
-  this.age = age;
-  this.isMale = isMale;
-
-  return this;
-}
-
-const cat4 = new Cat('Barsic', 'siameese', 3.5, true);
-
-// const cat5 = Cat('Ivan', 'dvorovoi', 50, true);
-
-/* 
-сделайте функцию конструктор Car
-у машины есть следубющие свойства
-марка
-модель
-цвет
-год выпуска
-расход топлива  (число, литров на километр)
-обьем бака (число)
-
-машина должна уметь ехать (метод обьекта)
+/*
+  Создать таблицу умножения в обьекте
 */
 
-function Car(
-  manufacturer,
-  model,
-  color,
-  releaseYear,
-  fuelConsumption,
-  fuelVolume
-) {
-  this.manufacturer = manufacturer;
-  this.model = model;
-  this.color = color;
-  this.releaseYear = releaseYear;
-  this.fuelConsumption = fuelConsumption;
-  this.fuelVolume = fuelVolume;
+function createMultiplicationtable() {
+  const table = {};
 
-  this.drive = function () {
-    return 'мы едем';
-  };
+  for (let i = 1; i <= 1000000; i++) {
+    for (let j = 1; j <= 10000000; j++) {
+      table[`${i} * ${j} = `] = i * j;
+    }
+  }
 
-  this.getDrivedDistance = function () {
-    return this.fuelVolume * this.fuelConsumption;
-  };
-
-  return this;
+  return table;
 }
 
-const car = new Car('Toyota', 'Camry', 'black', 2022, 8.3, 60);
+const tableButBetter = createMultiplicationtable();
 
-const car2 = new Car('Toyota', 'Camry', 'black', 2022, 15, 100);
-// const car3 = new Car('Toyota', 'Camry', 'black', 2022);
-
-car.test = function () {
-  // returns car
-  return this;
-}
-
-function tst() {
-  // undefined
-}
+const table = {
+  '1 * 1 = ': 1,
+  '1 * 2 = ': 2,
+  '1 * 3 = ': 3,
+  '1 * 4 = ': 4,
+  '1 * 5 = ': 5,
+  '1 * 6 = ': 6,
+  '1 * 7 = ': 7,
+  '1 * 8 = ': 8,
+  '1 * 9 = ': 9,
+  '1 * 10 =': 10,
+  '2 * 1 = ': 2,
+  '2 * 2 = ': 4,
+  '2 * 3 = ': 6,
+  '2 * 4 = ': 8,
+  '2 * 5 = ': 10,
+  '2 * 6 = ': 12,
+  '2 * 7 = ': 14,
+  '2 * 8 = ': 16,
+  '2 * 9 = ': 18,
+  '2 * 10 = ': 20,
+  '3 * 1 = ': 3,
+  '9 * 10 = ': 90,
+};
