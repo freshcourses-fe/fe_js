d6002c60d0f0f7e0c91247efa6d54aab8e7956c4
Kirill <freshjs.ftd.@gmail.com>
2022-02-04 20:36:26 +0200

    Add filter

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
 'use strict';
 
 const maleNames = ['Anton', 'Pavel', 'Ivan'];
-const femaleNames = ['Maria', 'Natasha', 'Masha'];
-const otherNames = ['Anton', 'Pavel', 'Ivan', 'Petka'];
+const femaleNames = ['Maria', 'Natasha', 'Masha', 'Anton'];
+const otherNames = ['Anton', 'Pavel', 'Anton', 'Petka'];
 
-// const callback = function (name, index, arr) {
-//   debugger;
-//   if (femaleNames === arr) {
-//     return `${name} Pupkina`;
-//   } else if (maleNames === arr) {
-//     return `${name} Pupkin`;
-//   }
-//   return `${name} Sirko`;
-// };
-const callback = function (name, index ) {
-  // debugger;
-  if (femaleNames.includes(name) ) {
-    return `${name} Pupkina`;
-  } else if (maleNames.includes(name) ) {
-    return `${name} Pupkin`;
-  }
-  return `${name} Sirko`;
-};
+const names = maleNames.concat(femaleNames, otherNames);
 
-const malesWithLastName = maleNames.map(callback);
+const witoutAntons = names.filter(callback);
 
-const femalesWithLastNames = femaleNames.map(callback);
+function callback(currentName, index, arr) {
+  return currentName !== 'Anton';
+}
 
-const othersWithLastName = otherNames.map(callback);
+const users = [
+  { name: 'Ivan', isAdult: true },
+  { name: 'Masha', isAdult: true },
+  { name: 'Anton', isAdult: false },
+];
 
-const names = malesWithLastName.concat(femalesWithLastNames, 'Igor Dudkin', othersWithLastName);
-
-// const newArr = names.map(maleCallback);
-
-// function
+const adults = users.filter(function(user, index, arr) {
+  return user.name === 'Anton';
+});
