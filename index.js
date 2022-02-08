2b0c34729720f7f0a0dca0b54a73105eb7b0f196
Kirill <freshjs.ftd.@gmail.com>
2022-02-08 18:32:21 +0200

    Add Moder and User inheritance

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function PhonePrototype() {
     return 'you can call abonents';
   };
 
-  this.endCall = function() {
+  this.endCall = function () {
     return 'Call ended';
-  }
+  };
 }
 
 // const phonePrototype = new PhonePrototype();
@@ -25,3 +25,82 @@ Phone.prototype = new PhonePrototype();
 
 const phone1 = new Phone('X', 'Iphone', 2018, 9999999);
 const phone2 = new Phone('Galaxy Note X', 'Samsung', 2015, 9999999);
+
+// const user1 = {
+//   name: 'Test',
+//   lastName: 'Testovich',
+// };
+
+// const user2 = {
+//   name: 'Undefined',
+//   lastName: 'Undefinenko',
+// };
+
+// const userPrototype = {
+//   name: 'Adam',
+//   lastName: 'Adamovich',
+//   getFullName: function () {
+//     return `${this.name} ${this.lastName}`;
+//   },
+//   walk: function () {
+//     return `${this.name} ${this.lastName} is walking`;
+//   },
+// };
+
+// user1.__proto__ = userPrototype;
+// user2.__proto__ = userPrototype;
+
+// const user3 = {};
+// user3.__proto__ = userPrototype;
+
+function User(name, lastName) {
+  this.name = name;
+  this.lastName = lastName;
+
+  return this;
+}
+
+function UserPrototype() {
+  this.name = 'Adam';
+  this.lastName = 'Adamovich';
+
+  this.getFullName = function () {
+    return `${this.name} ${this.lastName}`;
+  };
+
+  this.walk = function () {
+    return `${this.name} ${this.lastName} is walking`;
+  };
+
+  return this;
+}
+
+const userPrototype = new UserPrototype();
+
+User.prototype = userPrototype;
+
+function ModeratorPrototype() {
+  this.run = function () {
+    return `${this.name} ${this.lastName} is running`;
+  };
+
+  this.deleteMessage = function () {
+    return `Message is deleted`;
+  };
+}
+
+ModeratorPrototype.prototype = userPrototype;
+
+function Moderator(name, lastName) {
+  this.name = name;
+  this.lastName = lastName;
+
+  return this;
+}
+
+const moderatorPrototype = new ModeratorPrototype();
+
+Moderator.prototype = moderatorPrototype;
+
+const newUser = new User('Item', 'Itemov');
+const moder = new Moderator('Moder', 'MOderovich');
