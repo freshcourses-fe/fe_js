08e88b70d9116e5c52541d13ebd516feaa18a213
Kirill <freshjs.ftd.@gmail.com>
2022-02-16 20:59:22 +0200

    Add some OOP principle examples

--- index.js
+++ index.js
@@ -50,3 +50,60 @@ const snake = new Snake('Ефрасинья Петровна');
 const cat = new Cat('Симба');
 
 const trainer = new Trainer();
+
+function a(condition) {
+  if (condition) {
+    // логика 1 (цикл)
+    return `1`;
+  }
+
+  return `0`;
+}
+
+function createAnimals(amount = 1) {
+  const result = [];
+  for (let i = 0; i < amount; i++) {
+    const newAnimal = new Animal(`Animal  ${i + 1}`, `Name ${i + 1}`);
+    result.push(newAnimal);
+  }
+
+  return result;
+}
+
+
+
+function b(condition) {
+  if (condition) {
+    // логика 2 (switch)
+    return `tesfa`;
+  }
+
+  return `sadfdsfgdsg`;
+}
+
+function c(condition, options) {
+  if (condition) {
+    return options.successResult;
+  }
+
+  return options.failResult;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+': {
+      return sum(a, b);
+    }
+    case '-': {
+      return subtract(a, b);
+    }
+  }
+}
