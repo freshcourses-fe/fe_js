79534fc3d67dd3d4ce418a8acb6e319ba6746eed
Kirill <freshjs.ftd.@gmail.com>
2022-02-17 20:47:50 +0200

    Add algoritms

--- index.js
+++ index.js
@@ -1,109 +1,143 @@
 /*
-Создать класс Animal и унаследовать от него несколько других животных, 
-например льва  и змею 
-у них будут клички
-Они долны уметь есть двигатсья есть и говорить (издавать звуки)
-Создать класс дрессировщика, котрый будет иметь методы, 
-которые будут заствалять дрессируемое 
-животное делать то или иное действие
+  y = log x - логарифмическая
+  y = x  - линейная функция
+  y = x^2  - квадратичная
+  y = x^3 - кубическая
+  y = 2 ^ x - експоненциальная
+  y = x! - факториальная
+*/
+/*
+5! = 1 * 2 * 3 * 4  * 5
 */
 
-class Animal {
-  constructor(type, nickname) {
-    this.type = type;
-    this.nickname = nickname;
-  }
+/*
+  n - сколько было операций / входных данный
+  x - быстродействие
+  x = 10*n + 15 - линейная сложность
+  x = n^2 + 2 - квадратичная сложность
+  x = 2^n + 5n + 1500
+*/
 
-  speak() {
-    return `Животное ${this.nickname} издает звуки`;
-  }
+/*
+ n = 20000
+
+ x = 20000 * 10 + 15 = 200015
+ x = 400000002
+ x = 40 * 10^6020
+*/
+
+/*
+  x = n - линейная сложность
+  x = n^2 
+  x = 2^n 
+
+  O(n)
+  O(n^2)
+  O(2^n + n)
+*/
+
+// O (1)
+function isFirstElementOfArray(arr, element) {
+  return arr[0] === element;
 }
 
-class Snake extends Animal {
-  constructor(nickname) {
-    super('Змея', nickname);
-  }
+const arr = [0, 1, 2, 5, 7, 8, 9];
+const arr2 = Array(50000000)
+  .fill(null)
+  .map((_, index) => index);
+const number1 = 8;
+const number2 = 5;
 
-  speak() {
-    return `${this.type} ${this.nickname} шипит`;
-  }
-}
+console.time('O(1)');
+const result1 = isFirstElementOfArray(arr, number1);
+console.timeEnd('O(1)');
 
-class Cat extends Animal {
-  constructor(nickname) {
-    super('Кот', nickname);
-  }
+console.time('O(1) big array');
+const result2 = isFirstElementOfArray(arr2, number2);
+console.timeEnd('O(1) big array');
 
-  speak() {
-    return `${this.type} ${this.nickname} мяукает`;
-  }
-}
-
-class Trainer {
-  trainSpeech(animal) {
-    return animal.speak();
-  }
-}
-
-const animal = new Animal('животное', 'Пушок');
-const snake = new Snake('Ефрасинья Петровна');
-const cat = new Cat('Симба');
-
-const trainer = new Trainer();
-
-function a(condition) {
-  if (condition) {
-    // логика 1 (цикл)
-    return `1`;
-  }
-
-  return `0`;
-}
-
-function createAnimals(amount = 1) {
-  const result = [];
-  for (let i = 0; i < amount; i++) {
-    const newAnimal = new Animal(`Animal  ${i + 1}`, `Name ${i + 1}`);
-    result.push(newAnimal);
-  }
-
-  return result;
-}
-
-
-
-function b(condition) {
-  if (condition) {
-    // логика 2 (switch)
-    return `tesfa`;
-  }
-
-  return `sadfdsfgdsg`;
-}
-
-function c(condition, options) {
-  if (condition) {
-    return options.successResult;
-  }
-
-  return options.failResult;
-}
-
-function sum(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function calculator(a, b, operator) {
-  switch (operator) {
-    case '+': {
-      return sum(a, b);
-    }
-    case '-': {
-      return subtract(a, b);
+// O (n)
+function isElementInArray(arr, elem) {
+  for (let i = 0; i < arr.length; i++) {
+    if (elem === arr[i]) {
+      return true;
     }
   }
+
+  return false;
 }
+
+console.time('O(n) best case');
+const oNbest = isElementInArray(arr2, 0);
+console.timeEnd('O(n) best case');
+
+console.time('O(n) worst case');
+const oNworst = isElementInArray(arr2, 4999999);
+console.timeEnd('O(n) worst case');
+
+// O (n^2)
+
+function createMultiplicationTable(limit) {
+  const table = [];
+  for (let i = 0; i < limit; i++) {
+    for (let j = 0; j < limit; j++) {
+      table.push(`${i} * ${j} = ${i * j}`);
+    }
+  }
+
+  return table;
+}
+
+console.time('O(n^2) best case');
+const goodTable = createMultiplicationTable(10);
+console.timeEnd('O(n^2) best case');
+
+console.time('O(n^2) worst case');
+const badTable = createMultiplicationTable(5000);
+console.timeEnd('O(n^2) worst case');
+
+// const arr = [0, 1, 2, 5, 7, 8, 9];
+// O (log n)
+function binarySearch(arr, number) {
+  // debugger;\
+  let steps = 0;
+  let start = 0;
+  let end = arr.length - 1;
+
+  let center = Math.round((start + end) / 2);
+
+  while (true) {
+    steps++;
+    if (arr[center] === number) {
+      console.log(steps);
+      return center;
+    }
+
+    if (arr[center] > number) {
+      end = center;
+      center = Math.floor((start + end) / 2);
+    } else {
+      start = center;
+      center = Math.ceil((start + end) / 2);
+    }
+  }
+}
+
+/*
+
+  Создать функцию бинарный вопросник
+  функция будет принимать число
+  0 .... 100 numberGuesser(100)
+  функция по циклу спрашивает confirm-ом у пользователя
+  рпвильное ли число которое по середине сейчас
+  
+
+*/
+
+console.time('O(log n) best case');
+const goodSearch = binarySearch(arr2, 2500000);
+console.timeEnd('O(log n) best case');
+
+console.time('O(log n) worst case');
+const badSearch = binarySearch(arr2, 1);
+console.timeEnd('O(log n) worst case');
