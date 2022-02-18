39bc9140230143f5de25e5e896761f7f8d4c3e93
Kirill <freshjs.ftd.@gmail.com>
2022-02-18 19:40:37 +0200

    Add more examples

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const obj = {
-  firstName: '',
-  lastName: '',
+  firstName: 'test',
+  lastName: 'test',
   age: 10,
+  address: {
+    city: 'ZP',
+  },
 };
 
 console.log(obj.age);
@@ -58,10 +61,37 @@ console.log(nasaPc.cpus[0].name);
 
 const {
   ram,
-  hardDriveInfo: { amount, unit },
+  hardDriveInfo: { amount, unit }, // вложенная деструктуризация
   hardDriveInfo,
-  unit: ramUnit
+  unit: ramUnit, // переименование переменной
+  cpus: [pentiumCPU, second],
 } = nasaPc;
 
+const { name, clockSpeed } = pentiumCPU;
+
+const arr = [1235, 5645787, 89797, 7909, [1, 2, 3]];
+
+const [fist, ...restArr] = arr;
+
+const { age, address, ...user } = obj;
+
+// создать обьект с вложенностью и деструктуризировать его
 
 console.log(ram, ramUnit);
+
+const obj1 = {
+  _name: 'test',
+  // get name () {
+  //   return this._name
+  // },
+
+  // set name (value) {
+  //   this._name = value;
+  // }
+};
+
+function greetUser({ firstName, lastName }, second) {
+  for (let i = 0; i < 100; i++) {
+    return `Hello ${firstName} ${lastName} ${second}`;
+  }
+}
