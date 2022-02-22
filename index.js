4c2ea12df4d832fd36fc3a248c105c8193ca781f
Kirill <freshjs.ftd.@gmail.com>
2022-02-22 20:51:59 +0200

    Add recursion and JsDoc

--- index.js
+++ index.js
@@ -1,153 +1,129 @@
-const map = new Map();
-
-map.set('name', 'Test');
-map.set('123', true);
-map.set(123, false);
-map.set(123, 'fals21312321e');
-
-const data = map.get('name');
-const key = { name: 'Test' };
-map.set(key, { password: '12345admin' });
-
-const func = function () {
-  return 'hello';
-};
-
-const arr = [123];
-
-map.set(func, 1);
-
-map.set(arr, true);
-
-const map2 = new Map([
-  [1, 'test'],
-  [arr, 'new'],
-]);
-
-const isArrAKey = map2.has(arr);
-const isFuncAKey = map2.has(func);
-
-const delete1 = map.delete(arr);
-const delete2 = map.delete(arr);
-
-// map.clear();
-
-const keys = [];
-
-for (const key of map.keys()) {
-  keys.push(key);
-}
-
-const values = [];
-
-for (const value of map.values()) {
-  values.push(value);
-}
-
-const entries = [];
-
-for (const entry of map.entries()) {
-  entries.push(entry);
-}
-
-const mapCopy = new Map(entries);
-
-/*
-есть 2 юзера с полями
-id
-name 
-lastName
-
-есть 2 массива с сообщениями пользователя
-
-вам нужно создать функцию которая приймет 
-данные от юзера
-и вернет массив с его сообщениями
-
-*/
-
-const user1 = {
-  id: 5,
-  name: 'John',
-  lastName: 'Doe',
-};
-
-const user2 = {
-  id: 5047,
-  name: 'Jane',
-  lastName: 'Doe',
-};
-
-const user3 = {
-  id: 12,
-  name: 'John',
-  lastName: 'NotDoe',
-};
-
-const johnMessages = ['Hi', 'Bye'];
-
-const janeMessages = ['Hi', 'Bye', 'NO', 'Yes'];
-
-const usersToMessages = new Map();
-
-usersToMessages.set(user1.id, johnMessages);
-usersToMessages.set(user2.id, janeMessages);
-
-function getUserMessages(userId) {
-  if (usersToMessages.has(userId)) {
-    return usersToMessages.get(userId);
-  }
-
-  throw new Error('404 User not found');
-}
-
-const getUserMessagesArrow = (userId) =>
-  usersToMessages.has(userId)
-    ? usersToMessages.get(userId)
-    : new Error('404 User not found');
-
-/*
-  есть строка текста на языке. 
-  Получить на её основании 
-  строку текста на другом языке
-*/
-const untranslatedText = 'Привет меня зовут Вася';
-const translatedText = 'Hello my name is Vasya';
-
-const dictionary = new Map([
-  ['привет', 'hello'],
-  ['меня', 'my'],
-  ['зовут', 'name is'],
-  ['вася', 'Vasya'],
-  ['кот', 'cat'],
-  ['собака', 'dog'],
-]);
-
-// function translate(...words) {
-//   let translated = '';
-//   for (let i = 0; i < words.length; i++) {
-//     translated += `${dictionary.get(words[i])} `;
-//   }
-
-//   return translated;
+// let counter = 0;
+// function myFunc() {
+//   debugger;
+//   counter++;
+//   myFunc();
 // }
 
-function translate(str) {
-  const words = str.toLowerCase().split(' ');
-
-  const result = [];
-
-  for (const word of words) {
-    if (dictionary.has(word)) {
-      result.push(dictionary.get(word));
-    } else {
-      result.push(word);
-    }
-  }
-
-  result[0] = result[0].toUpperCase();
-
-  return result.join(' ');
+for (let i = 10; i > 0; i--) {
+  console.log(i);
 }
 
-const res = translate('Привет', 'меня', 'зовут', 'Вася');
+console.log('Go');
+
+// function countdown(number = 10) {
+//   debugger;
+//   console.log(number);
+//   if (number > 1) {
+//     countdown(number - 1);
+//   } else {
+//     console.log('start');
+//   }
+// }
+
+function toPower(number = 5, exp = 2) {
+  // debugger;
+  // if (exp === 1) {
+  //   return number;
+  // } else if (exp > 1) {
+  //   return number * toPower(number, exp - 1);
+  // } else if (exp === 0) {
+  //   return 1;
+  // } else {
+  //   return 1 / (number * toPower(number, exp * -1 - 1));
+  // }
+
+  if (exp === 1) {
+    return number;
+  } else if (exp === 0) {
+    return 1;
+  }
+
+  return exp > 1
+    ? number * toPower(number, exp - 1)
+    : 1 / (number * toPower(number, Math.abs(exp) - 1));
+}
+
+// написать рекрсивную функцию
+// нахождение факториала числа
+
+// 5! = 1 * 2 * 3 * 4 * 5 => 120
+function getFactorial(number = 2) {
+  if (number < 0) {
+    throw new RangeError('Factorial of negative numbers doesnt exist');
+  }
+
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+
+  return number * getFactorial(number - 1);
+}
+
+// переделать toPower так
+// чтобы он мог принимать отрицательные степени числа
+
+const binaryTree = {
+  value: 1,
+  left: {
+    value: 5,
+    left: {
+      value: 2,
+    },
+  },
+  right: {
+    value: 3,
+    left: {
+      value: 8,
+      right: {
+        value: -5,
+      },
+    },
+    right: {
+      value: 20,
+    },
+  },
+};
+
+/**
+ *
+ * @param {object} tree
+ * @param {number} tree.value - значение узла
+ * @param {object} tree.left - узел слева
+ * @param {object} tree.right - узел справа
+ * @returns {number} сумма дерева
+ */
+function getTreeSum(tree) {
+  let result = tree.value;
+
+  if (tree.left) {
+    result += getTreeSum(tree.left);
+  }
+
+  if (tree.right) {
+    result += getTreeSum(tree.right);
+  }
+
+  return result;
+}
+
+/**
+ * Эта функция складывает 2 числа
+ * @param {number} a - число коорое возводим
+ * @param {number} b - степень
+ * @returns {number} результат возведения a в степень b
+ */
+function toPower2(a, b) {
+  return a ** b;
+}
+
+toPower2('test', 2);
+
+/**
+ *
+ * @param  {number[]} numbers - массив с числами
+ * @returns
+ */
+function sum(numbers) {
+  return numbers.reduce((sum, currentValue) => sum + currentValue);
+}
